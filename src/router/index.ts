99d03525108d968d3f8ccd17e8b16ea0345d2543
jh8057 <luckysymbol13@gmail.com>
2022-10-16 00:23:37 +0900

    [route] remove

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,31 +9,6 @@ const router = createRouter({
       name: "home",
       component: HomeView,
     },
-    {
-      path: "/userInfoPage",
-      name: "userInfoPage",
-      component: () => import("../views/userInfoPage.vue"),
-    },
-    {
-      path: "/orderPage",
-      name: "orderPage",
-      component: () => import("../views/orderPage.vue"),
-    },
-    {
-      path: "/orderTwoPage",
-      name: "orderTwoPage",
-      component: () => import("../views/orderTwoPage.vue"),
-    },
-    {
-      path: "/testOnePage",
-      name: "testOnePage",
-      component: () => import("../views/testOnePage.vue"),
-    },
-    {
-      path: "/testTwoPage",
-      name: "testTwoPage",
-      component: () => import("../views/testTwoPage.vue"),
-    },
   ],
 });
 
